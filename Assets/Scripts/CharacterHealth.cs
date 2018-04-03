93d97b87fe5528a09a0d0343256cd4d3a26d6489
Pulkkinen Lauri <Lauri.Pulkkinen@joutsenonopisto.fi>
2018-04-03 15:09:31 +0300

    Adding untracked files

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CharacterHealth : MonoBehaviour {
+
+     float maxHealth;
+     float curHealth;
+    public Slider healthBar;
+
+    
+    // Use this for initialization
+    void Start()
+    {
+
+        maxHealth = 1000F;
+
+        curHealth = maxHealth;
+
+        healthBar.value = CalculateHealth();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+
+    }
+
+    float CalculateHealth()
+    {
+        return curHealth / maxHealth;
+
+    }
+
+   public void TakeSomeDamage(float damage)
+    {
+
+        if (curHealth <= 0F)
+        {
+            Die();
+        }
+
+        curHealth -= damage;
+
+        healthBar.value = CalculateHealth();
+    }
+
+    public void Die()
+    {
+        curHealth = 0;
+        SceneManager.LoadScene(0);
+
+    }
+
+  }
+
---
+++ Assets/Scripts/DamageControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DamageControl : MonoBehaviour
+{
+    public float damage;
+    public GameObject chrhealth;
+     CharacterHealth chr_script;
+    // Use this for initialization
+    void Start()
+    {
+        chr_script = chrhealth.GetComponent<CharacterHealth>();
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+            {
+            if (this.gameObject.tag == "Hand_R")
+            {
+                damage = 15F;
+                chr_script.TakeSomeDamage(damage);
+            }
+            if(this.gameObject.tag == "Foot_R")
+            {
+                damage = 30F;
+                chr_script.TakeSomeDamage(damage);
+            }
+        }
+    }
+}
