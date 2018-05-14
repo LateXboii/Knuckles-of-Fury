7dda2320d1c76e830f3685c696025eb023fb240b
Kotilainen Sirumilla <Sirumilla.Kotilainen@joutsenonopisto.fi>
2018-05-14 13:54:19 +0300

    Added scrolling parallax camera script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, 0, -1);
+        }
+	}
+}
